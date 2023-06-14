d9db78a69e1186a8ca392ae958665309ac8d7241
Tom Valentino <tomvalentino2019@gmail.com>
2023-06-15 01:10:46 +0800

    jknjn

--- src/Firebase.js
+++ src/Firebase.js
@@ -7,22 +7,13 @@ import "firebase/compat/firestore"
 
 
 const app = firebase.initializeApp({
-
-<<<<<<< HEAD
-    apiKey: "AIzaSyCgXruWrn1iq3v1FW8wBQj8jfiiBPafgS0",
-    authDomain: "genius-builder-80de2.firebaseapp.com",
-    projectId: "genius-builder-80de2",
-    storageBucket: "genius-builder-80de2.appspot.com",
-    messagingSenderId: "683068244368",
-    appId: "1:683068244368:web:b3d80574b2c111b43d3315"
-=======
-apiKey: "AIzaSyBjJ-t_U6ghISi6dTU8ZKF274pBDZADGZE",
-  authDomain: "test-bb5a1.firebaseapp.com",
-  projectId: "test-bb5a1",
-  storageBucket: "test-bb5a1.appspot.com",
-  messagingSenderId: "1090832818342",
-  appId: "1:1090832818342:web:c03b1f099b753e3c505467"
->>>>>>> 7aa549b (New comits)
+    
+    apiKey: "AIzaSyBjJ-t_U6ghISi6dTU8ZKF274pBDZADGZE",
+    authDomain: "test-bb5a1.firebaseapp.com",
+    projectId: "test-bb5a1",
+    storageBucket: "test-bb5a1.appspot.com",
+    messagingSenderId: "1090832818342",
+    appId: "1:1090832818342:web:c03b1f099b753e3c505467"
 
 
 })
